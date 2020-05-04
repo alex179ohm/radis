bd039e76aac618910ee679cc0fb78cd9e67e27e4
Alessandro Cresto Miseroglio <alex179ohm@gmail.com>
2020-05-04 22:04:18 +0200

    Fix test.

--- src/cmd.rs
+++ src/cmd.rs
@@ -23,7 +23,7 @@ impl From<CmdEncodingError> for io::Error {
 /// # Examples
 /// ```rust
 /// # use radis::Cmd;
-/// let pass = "mysecretpass";
+/// let pass = "pass";
 /// let cmd = Cmd::new("AUTH").arg(pass).build().expect("failed to create auth command");
 /// assert_eq!(&b"*2\r\n$4\r\nAUTH\r\n$4\r\npass\r\n"[..], cmd.as_ref());
 /// ```
